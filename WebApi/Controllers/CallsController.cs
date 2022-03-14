532227390a52820e7790dfa6ff210789b53b498b
Antonis Ioannou <antonios.ioannou@protonmail.com>
2022-03-14 15:27:08 +0200

    Add project files.

---
+++ WebApi/Controllers/CallsController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApi.Models;
+
+namespace WebApi.Controllers
+{
+    public class CallsController : ApiController
+    {
+        private CallManagementEntities db = new CallManagementEntities();
+
+        // GET: api/Products
+        public IQueryable<Calls> GetCalls()
+        {
+            return db.Calls;
+        }
+
+
+    }
+}
