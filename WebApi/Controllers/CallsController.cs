35f4e0cadf230a26a7544fb46614f9883f9a2ca5
Antonis Ioannou <antonios.ioannou@protonmail.com>
2022-03-15 10:49:04 +0200

    updates

--- WebApi/Controllers/CallsController.cs
+++ WebApi/Controllers/CallsController.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using System.Web.Http.Description;
 using WebApi.Models;
 
 namespace WebApi.Controllers
@@ -18,6 +21,97 @@ namespace WebApi.Controllers
             return db.Calls;
         }
 
+        // GET: api/Products/5
+        [ResponseType(typeof(Calls))]
+        public IHttpActionResult GetProduct(int id)
+        {
+            Calls call = db.Calls.Find(id);
+            if (call == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(call);
+        }
+
+        // PUT: api/Products/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutProduct(int id, Calls call)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != call.CallsId)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(call).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ProductExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Products
+        [ResponseType(typeof(Calls))]
+        public IHttpActionResult PostProduct(Calls call)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Calls.Add(call);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = call.CallsId }, call);
+        }
+
+        // DELETE: api/Products/5
+        [ResponseType(typeof(Calls))]
+        public IHttpActionResult DeleteProduct(int id)
+        {
+            Calls call = db.Calls.Find(id);
+            if (call == null)
+            {
+                return NotFound();
+            }
+
+            db.Calls.Remove(call);
+            db.SaveChanges();
+
+            return Ok(call);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool ProductExists(int id)
+        {
+            return db.Calls.Count(e => e.CallsId == id) > 0;
+        }
 
     }
 }
---
+++ WebApp/Controllers/CallsController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Net;
+
+namespace WebApp.Controllers
+{
+    public class CallsController : Controller
+    {
+        // GET: Calls
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
