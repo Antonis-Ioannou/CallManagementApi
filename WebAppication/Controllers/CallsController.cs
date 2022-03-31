684ed55e9d62bde2d8f0c34511575a669fe3c9c4
Antonis Ioannou <antonios.ioannou@protonmail.com>
2022-03-31 16:18:18 +0300

    updates

---
+++ WebAppication/Controllers/CallsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebAppication.Controllers
+{
+    public class CallsController : Controller
+    {
+        // GET: Calls
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
